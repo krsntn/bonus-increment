3871677c51275a2791841e52d4ad6664a0f7214f
krsntn <kanahan0110@gmail.com>
2023-05-09 17:47:38 +0800

    add desc

--- src/App.jsx
+++ src/App.jsx
@@ -36,121 +36,126 @@ function App() {
   };
 
   return (
-    <div style={{ display: "flex", gap: "10rem" }}>
-      <div>
-        <form
-          onSubmit={calcBonus}
-          style={{
-            marginBottom: "1rem",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
-            gap: "0.3rem",
-          }}
-        >
-          <div
+    <>
+      <div style={{ fontSize: "0.8rem", marginBottom: "2rem" }}>
+        Rest assured that no data will be collected through this website.
+      </div>
+      <div style={{ display: "flex", gap: "10rem" }}>
+        <div>
+          <form
+            onSubmit={calcBonus}
             style={{
-              alignSelf: "center",
-              textTransform: "uppercase",
-              fontWeight: "bold",
               marginBottom: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+              gap: "0.3rem",
             }}
           >
-            calculte bonus
-          </div>
-          <div>
-            base salary:{" "}
-            <input
-              type={"text"}
-              value={salary}
-              onChange={(e) => setSalary(e.target.value)}
-              style={{ padding: "0.3rem" }}
-              pattern="[0-9]+"
-            />
-          </div>
-          <button
-            type="submit"
-            style={{ width: "100%", backgroundColor: "#3b82f6" }}
-          >
-            calculate
-          </button>
-        </form>
+            <div
+              style={{
+                alignSelf: "center",
+                textTransform: "uppercase",
+                fontWeight: "bold",
+                marginBottom: "1rem",
+              }}
+            >
+              calculte bonus
+            </div>
+            <div>
+              base salary:{" "}
+              <input
+                type={"text"}
+                value={salary}
+                onChange={(e) => setSalary(e.target.value)}
+                style={{ padding: "0.3rem" }}
+                pattern="[0-9]+"
+              />
+            </div>
+            <button
+              type="submit"
+              style={{ width: "100%", backgroundColor: "#3b82f6" }}
+            >
+              calculate
+            </button>
+          </form>
 
-        {bonuses.length > 0 && (
-          <table style={{ textAlign: "right" }}>
-            <thead>
-              <tr>
-                <th style={{ paddingLeft: "3rem" }}>Month</th>
-                <th style={{ paddingLeft: "3rem" }}>Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bonuses.map((x, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{x[0]}</td>
-                    <td>{x[1]}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
-      </div>
+          {bonuses.length > 0 && (
+            <table style={{ textAlign: "right" }}>
+              <thead>
+                <tr>
+                  <th style={{ paddingLeft: "3rem" }}>Month</th>
+                  <th style={{ paddingLeft: "3rem" }}>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {bonuses.map((x, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{x[0]}</td>
+                      <td>{x[1]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
 
-      <div>
-        <form
-          onSubmit={calcIncrement}
-          style={{
-            marginBottom: "1rem",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
-            gap: "0.3rem",
-          }}
-        >
-          <div
+        <div>
+          <form
+            onSubmit={calcIncrement}
             style={{
-              alignSelf: "center",
-              textTransform: "uppercase",
-              fontWeight: "bold",
               marginBottom: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+              gap: "0.3rem",
             }}
           >
-            calculte increment
-          </div>
-          <div>
-            prev salary:{" "}
-            <input
-              type={"text"}
-              value={prevSalary}
-              onChange={(e) => setPrevSalary(e.target.value)}
-              style={{ padding: "0.3rem" }}
-              pattern="[0-9]+"
-            />
-          </div>
+            <div
+              style={{
+                alignSelf: "center",
+                textTransform: "uppercase",
+                fontWeight: "bold",
+                marginBottom: "1rem",
+              }}
+            >
+              calculte increment
+            </div>
+            <div>
+              prev salary:{" "}
+              <input
+                type={"text"}
+                value={prevSalary}
+                onChange={(e) => setPrevSalary(e.target.value)}
+                style={{ padding: "0.3rem" }}
+                pattern="[0-9]+"
+              />
+            </div>
 
-          <div>
-            new salary:{" "}
-            <input
-              type={"text"}
-              value={newSalary}
-              onChange={(e) => setNewSalary(e.target.value)}
-              style={{ padding: "0.3rem" }}
-              pattern="[0-9]+"
-            />
-          </div>
-          <button
-            type="submit"
-            style={{ width: "100%", backgroundColor: "#3b82f6" }}
-          >
-            calculate
-          </button>
-        </form>
+            <div>
+              new salary:{" "}
+              <input
+                type={"text"}
+                value={newSalary}
+                onChange={(e) => setNewSalary(e.target.value)}
+                style={{ padding: "0.3rem" }}
+                pattern="[0-9]+"
+              />
+            </div>
+            <button
+              type="submit"
+              style={{ width: "100%", backgroundColor: "#3b82f6" }}
+            >
+              calculate
+            </button>
+          </form>
 
-        {increment && <div>{increment}%</div>}
+          {increment && <div>{increment}%</div>}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
